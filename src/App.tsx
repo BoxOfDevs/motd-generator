1e16d4cc5c989da326bb4beb7e209037605b33e5
Nathaniel Fredericks <me@nathfreder.dev>
2019-08-24 18:18:56 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const SELECTION_SIGN = '§';
+
+const colorCodes = {
+  0: 'color: #000000', // black
+  1: 'color: #0000AA', // dark_blue
+  2: 'color: #00AA00', // dark_green
+  3: 'color: #00AAAA', // dark_aqua
+  4: 'color: #AA0000', // dark_red
+  5: 'color: #AA00AA', // dark_purple
+  6: 'color: #FFAA00', // gold
+  7: 'color: #AAAAAA', // gray
+  8: 'color: #555555', // dark_gray
+  9: 'color: #5555FF', // blue
+  a: 'color: #55FF55', // green
+  b: 'color: #55FFFF', // aqua
+  c: 'color: #FF5555', // red
+  d: 'color: #FF55FF', // light_purple
+  e: 'color: #FFFF55', // yellow
+  f: 'color: #FFFFFF' // white
+};
+
+const formattingCodes = {
+  l: 'font-weight: bold;', // Bold
+  m: 'text-decoration: line-through;', // Strikethrough
+  n: 'text-decoration: underline;', // Underline
+  o: 'font-style: italic;' // Italic
+};
+
+const allCodes = {
+  ...colorCodes,
+  ...formattingCodes
+}
+
+class App extends React.Component {
+  state = {
+    motd: ''
+  }
+
+  handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    let motd = event.currentTarget.value;
+
+    for (const [key, value] of Object.entries(allCodes)) {
+      motd = motd.replace(`${SELECTION_SIGN}${key}`, `<span style="${value}">`);
+      motd = motd.replace(`${SELECTION_SIGN}r`, `</span>`);
+    }
+
+    this.setState({
+      motd,
+    })
+  }
+
+  render = () => (
+    <div className="App">
+      <input type="text" onChange={this.handleChange} />
+      <p dangerouslySetInnerHTML={{ __html: this.state.motd }}></p>
+    </div>
+  );
+}
+
+export default App;
