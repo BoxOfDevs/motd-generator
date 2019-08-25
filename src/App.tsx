c731dcd94e99717bdfdf57598693dbdbb61f03c6
Nathaniel Fredericks <me@nathfreder.dev>
2019-08-25 17:16:12 -0300

    Add color picker and refactor codes

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import logo from './logo.svg';
 const SELECTION_SIGN = '§';
 
 const colorCodes = {
-  0: 'color: #000000', // black
-  1: 'color: #0000AA', // dark_blue
-  2: 'color: #00AA00', // dark_green
-  3: 'color: #00AAAA', // dark_aqua
-  4: 'color: #AA0000', // dark_red
-  5: 'color: #AA00AA', // dark_purple
-  6: 'color: #FFAA00', // gold
-  7: 'color: #AAAAAA', // gray
-  8: 'color: #555555', // dark_gray
-  9: 'color: #5555FF', // blue
-  a: 'color: #55FF55', // green
-  b: 'color: #55FFFF', // aqua
-  c: 'color: #FF5555', // red
-  d: 'color: #FF55FF', // light_purple
-  e: 'color: #FFFF55', // yellow
-  f: 'color: #FFFFFF', // white
-  r: 'color: initial; font-weight: initial; text-decoration: initial; font-style: initial;'
+  0: '#000000', // black
+  1: '#0000AA', // dark_blue
+  2: '#00AA00', // dark_green
+  3: '#00AAAA', // dark_aqua
+  4: '#AA0000', // dark_red
+  5: '#AA00AA', // dark_purple
+  6: '#FFAA00', // gold
+  7: '#AAAAAA', // gray
+  8: '#555555', // dark_gray
+  9: '#5555FF', // blue
+  a: '#55FF55', // green
+  b: '#55FFFF', // aqua
+  c: '#FF5555', // red
+  d: '#FF55FF', // light_purple
+  e: '#FFFF55', // yellow
+  f: '#FFFFFF' // white
 };
 
 const formattingCodes = {
@@ -31,31 +30,57 @@ const formattingCodes = {
 };
 
 const allCodes = {
-  ...colorCodes,
-  ...formattingCodes
+  ...Object.fromEntries(Object.entries(colorCodes).map(([key, val]) => [ key, `color: ${val}`])),
+  ...formattingCodes,
+  r: 'color: initial; font-weight: initial; text-decoration: initial; font-style: initial;'
 }
+
 
 class App extends React.Component {
   state = {
-    motd: ''
+    htmlMotd: '',
+    rawMotd: '',
+    colorButtons: null
+  }
+
+  componentDidMount = () => {
+    const colorButtons = Object.entries(colorCodes).map(([ key, val ]) => (
+      <span style={{backgroundColor: val, marginRight: 5, paddingLeft: '24px', paddingTop: '5px', border: '1px solid black'}} onClick={() => this.insertCode(key)} />
+    ));
+    this.setState({
+      colorButtons,
+    })
   }
 
   handleChange = (event: React.FormEvent<HTMLTextAreaElement>) => {
-    let motd = event.currentTarget.value;
+    const rawMotd = event.currentTarget.value;
+    this.renderMotd(rawMotd);
+  }
 
-    for (const [key, value] of Object.entries(allCodes)) {
-      motd = motd.replace(`${SELECTION_SIGN}${key}`, `<span style="${value}">`);
+  renderMotd = (rawMotd: string) => {
+    let htmlMotd = rawMotd;
+
+    for (const [key, val] of Object.entries(allCodes)) {
+      htmlMotd = htmlMotd.replace(`${SELECTION_SIGN}${key}`, `<span style="${val}">`);
     }
 
     this.setState({
-      motd,
+      htmlMotd: htmlMotd,
+      rawMotd: rawMotd
     })
+  }
+
+  insertCode = (code: string) => {
+    let oldRawMotd = this.state.rawMotd;
+    oldRawMotd += `${SELECTION_SIGN}${code}`
+    this.renderMotd(oldRawMotd);
   }
 
   render = () => (
     <div>
-      <textarea placeholder={`${SELECTION_SIGN}cMy awesome MOTD`} onChange={this.handleChange} />
-      <p dangerouslySetInnerHTML={{ __html: this.state.motd }}></p>
+      {this.state.colorButtons}
+      <textarea autoFocus style={{marginTop: 8}} placeholder={`${SELECTION_SIGN}cMy awesome MOTD`} value={this.state.rawMotd} onChange={this.handleChange} />
+      <p className="preview" dangerouslySetInnerHTML={{ __html: this.state.htmlMotd }}></p>
     </div>
   );
 }
